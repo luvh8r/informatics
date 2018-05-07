f3ac4171c071713b017a8ce4c2ca5fa0d0d17723
Luv H8R <36643761+luvh8r@users.noreply.github.com>
2018-05-07 19:59:57 +0300

    Update search.php

--- homework04/search.php
+++ homework04/search.php
@@ -7,13 +7,13 @@ require_once 'connect.php';
 $connection = mysqli_connect($host, $user, $password, $db);
 
 
-$full_name = strtr($_GET['full_name'], '*', '%');
-$number = strtr($_GET['number'], '*', '%');
 echo "<form method='GET' action='search.php'>
 <p>Введите полное имя клиента: <input type='text' name='full_name' value='$full_name'></p>
 <p>Введите номер клиента: <input type='text' name='number' value='$number'></p>
 <p><input type='submit' name='enter' value='Поиск'></p>
 </form>";
+$full_name = strtr($_GET['full_name'], '*', '%');
+$number = strtr($_GET['number'], '*', '%');
 
 
 if (isset($_GET['enter'])) {
